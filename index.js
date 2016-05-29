c995e68269bd399e920652430a1fdc81cd533417
Aaron Franks <aaron.franks@gmail.com>
2016-05-29 14:50:10 -0700

    Extract validation logic to validateVar()

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ EnvError.prototype = Object.create(Error.prototype)
 EnvError.prototype.name = 'EnvError'
 exports.EnvError = EnvError
 
+
+function validateVar({ spec, name, value }) {
+    if (spec.choices) {
+        if (!Array.isArray(spec.choices)) {
+            throw new Error(`"choices" must be an array (in spec for "${name}")`)
+        } else if (!spec.choices.includes(value)) {
+            throw new EnvError(`Env var "${name}" not in choices [${spec.choices}]`)
+        }
+    }
+    if (value == null) throw new EnvError(`Missing env var "${name}"`)
+    return value
+}
+
+
 exports.lockEnv = function lockEnv(env, specs = {}, options = {}) {
     const output = {}
     const varKeys = Object.keys(specs)
@@ -13,16 +27,7 @@ exports.lockEnv = function lockEnv(env, specs = {}, options = {}) {
     for (const k of varKeys) {
         const spec = specs[k]
         const value = env[k] || spec.default
-
-        if (spec.choices) {
-            if (!Array.isArray(spec.choices)) {
-                throw new Error(`"choices" must be an array (in spec for "${k}")`)
-            } else if (!spec.choices.includes(value)) {
-                throw new EnvError(`Env var "${k}" not in choices [${spec.choices}]`)
-            }
-        }
-        if (value == null) throw new EnvError(`Missing env var "${k}"`)
-        output[k] = value
+        output[k] = validateVar({ name: k, spec, value })
     }
 
     return options.strict
