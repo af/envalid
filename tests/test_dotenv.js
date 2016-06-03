c6b9223bf6ae1ff8e3c23c58c863f008ea5db225
Aaron Franks <aaron.franks@gmail.com>
2016-06-03 13:15:20 -0700

    Add some tests for dotenv integration

---
+++ tests/test_dotenv.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const { createGroup, assert } = require('painless')
+const { cleanEnv, EnvError, str, num } = require('..')
+const test = createGroup()
+
+test.beforeEach(() => fs.writeFileSync('.env', `
+BAR=asdfasdf
+MYNUM=4
+`))
+test.afterEach(() => fs.unlinkSync('.env'))
+
+
+test('.env contents are cleaned', () => {
+    const env = cleanEnv({ FOO: 'bar' }, {
+        FOO: str(),
+        MYNUM: num()
+    })
+    assert.deepEqual(env, { FOO: 'bar', BAR: 'asdfasdf', MYNUM: 4 })
+})
+
+test('.env test in strict mode', () => {
+    const opts = { strict: true }
+    const env = cleanEnv({ FOO: 'bar', BAZ: 'baz' }, {
+        MYNUM: num()
+    }, opts)
+    assert.deepEqual(env, { MYNUM: 4 })
+})
