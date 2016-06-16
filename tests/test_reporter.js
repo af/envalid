2c3969557ec704ec367b7731026d388f0f513cec
Aaron Franks <aaron.franks@gmail.com>
2016-06-15 21:34:21 -0700

    Use chalk for slightly nicer output with colours

--- tests/test_reporter.js
+++ tests/test_reporter.js
@@ -22,9 +22,9 @@ test('reporter', () => {
     assert.strictEqual(logSpy.callCount, 1)
 
     const output = logSpy.firstCall.args[0]
-    assert.match(output, /Invalid environment variable/)
-    assert.match(output, /FOO: hi/)
-    assert.notMatch(output, /Missing environment variables:/)
+    assert.match(output, /Invalid\S+ environment variable/)
+    assert.match(output, /FOO\S+: hi/)
+    assert.notMatch(output, /Missing\S+ environment variables:/)
 
     assert.strictEqual(exitSpy.callCount, 1)
     assert(exitSpy.firstCall.calledWithExactly(1))
