4272c0b463b17c0a79a5183dd65e12fac2ef0520
Aaron Franks <aaron.franks@gmail.com>
2019-10-31 20:52:02 -0700

    Remove use of very out-of-date typescript-definition-tester package

--- tests/test_typescript.js
+++
@@ -1,7 +0,0 @@
-const { createGroup } = require('painless')
-const tt = require('typescript-definition-tester')
-const test = createGroup()
-
-test('Typescript declaration', done => {
-    tt.compileDirectory(__dirname, fileName => fileName.indexOf('.ts') > -1, () => done())
-})
