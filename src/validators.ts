c9210830f13ae9c2cdcdebad8335b0dbeb4a9020
Aaron Franks <aaron.franks@gmail.com>
2020-10-04 19:56:06 -0700

    Bring back type parameter support for validator functions

--- src/validators.ts
+++ src/validators.ts
@@ -16,7 +16,6 @@ const isFQDN = (input: string) => {
 // "best effort" regex-based IP address check
 // If you want a more exhaustive check, create your own custom validator, perhaps wrapping this
 // implementation (the source of the ipv4 regex below): https://github.com/validatorjs/validator.js/blob/master/src/lib/isIP.js
-// TODO write tests for this
 const ipv4Regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
 const ipv6Regex = /([a-f0-9:]+:+)+[a-f0-9]+/
 const isIP = (input: string) => {
@@ -27,16 +26,16 @@ const isIP = (input: string) => {
 const EMAIL_REGEX = /^[^@\s]+@[^@\s]+\.[^@\s]+$/ // intentionally non-exhaustive
 
 export class EnvError extends TypeError {
-  constructor(...args: any[]) {
-    super(...args)
+  constructor(message?: string) {
+    super(message)
     Error.captureStackTrace(this, EnvError)
     this.name = 'EnvError'
   }
 }
 
 export class EnvMissingError extends ReferenceError {
-  constructor(...args: any[]) {
-    super(...args)
+  constructor(message?: string) {
+    super(message)
     Error.captureStackTrace(this, EnvMissingError)
     this.name = 'EnvMissingError'
   }
@@ -48,74 +47,89 @@ export const makeValidator = <T>(parseFn: (input: string) => T, type: string = '
   }
 }
 
-// FIXME: look into type parameter stuff from old libdefs
-export const bool = makeValidator((input: string | boolean) => {
-  switch (input) {
-    case true:
-    case 'true':
-    case 't':
-    case '1':
-      return true
-    case false:
-    case 'false':
-    case 'f':
-    case '0':
-      return false
-    default:
-      return null
-  }
-}, 'bool')
+export function bool<T extends boolean = boolean>(spec?: Spec<T>) {
+  return makeValidator((input: string | boolean) => {
+    switch (input) {
+      case true:
+      case 'true':
+      case 't':
+      case '1':
+        return true
+      case false:
+      case 'false':
+      case 'f':
+      case '0':
+        return false
+      default:
+        return null
+    }
+  }, 'bool')(spec)
+}
 
-export const num = makeValidator((input: string) => {
-  const coerced = +input
-  if (Number.isNaN(coerced)) throw new EnvError(`Invalid number input: "${input}"`)
-  return coerced
-}, 'num')
+export function num<T extends number = number>(spec?: Spec<T>) {
+  return makeValidator((input: string) => {
+    const coerced = +input
+    if (Number.isNaN(coerced)) throw new EnvError(`Invalid number input: "${input}"`)
+    return coerced
+  }, 'num')(spec)
+}
 
-export const str = makeValidator((input: string) => {
-  if (typeof input === 'string') return input
-  throw new EnvError(`Not a string: "${input}"`)
-}, 'str')
+export function str<T extends string = string>(spec?: Spec<T>) {
+  return makeValidator((input: string) => {
+    if (typeof input === 'string') return input
+    throw new EnvError(`Not a string: "${input}"`)
+  }, 'str')(spec)
+}
 
-export const email = makeValidator((x: string) => {
-  if (EMAIL_REGEX.test(x)) return x
-  throw new EnvError(`Invalid email address: "${x}"`)
-}, 'email')
+export function email<T extends string = string>(spec?: Spec<T>) {
+  return makeValidator((x: string) => {
+    if (EMAIL_REGEX.test(x)) return x
+    throw new EnvError(`Invalid email address: "${x}"`)
+  }, 'email')(spec)
+}
 
-export const host = makeValidator((input: string) => {
-  if (!isFQDN(input) && !isIP(input)) {
-    throw new EnvError(`Invalid host (domain or ip): "${input}"`)
-  }
-  return input
-}, 'host')
+export function host<T extends string = string>(spec?: Spec<T>) {
+  return makeValidator((input: string) => {
+    if (!isFQDN(input) && !isIP(input)) {
+      throw new EnvError(`Invalid host (domain or ip): "${input}"`)
+    }
+    return input
+  }, 'host')(spec)
+}
 
-export const port = makeValidator((input: string) => {
-  const coerced = +input
-  if (
-    Number.isNaN(coerced) ||
-    `${coerced}` !== `${input}` ||
-    coerced % 1 !== 0 ||
-    coerced < 1 ||
-    coerced > 65535
-  ) {
-    throw new EnvError(`Invalid port input: "${input}"`)
-  }
-  return coerced
-}, 'port')
+export function port<T extends number = number>(spec?: Spec<T>) {
+  return makeValidator((input: string) => {
+    const coerced = +input
+    if (
+      Number.isNaN(coerced) ||
+      `${coerced}` !== `${input}` ||
+      coerced % 1 !== 0 ||
+      coerced < 1 ||
+      coerced > 65535
+    ) {
+      throw new EnvError(`Invalid port input: "${input}"`)
+    }
+    return coerced
+  }, 'port')(spec)
+}
 
-export const url = makeValidator((x: string) => {
-  try {
-    new URL(x)
-    return x
-  } catch (e) {
-    throw new EnvError(`Invalid url: "${x}"`)
-  }
-}, 'url')
+export function url<T extends string = string>(spec?: Spec<T>) {
+  return makeValidator((x: string) => {
+    try {
+      new URL(x)
+      return x
+    } catch (e) {
+      throw new EnvError(`Invalid url: "${x}"`)
+    }
+  }, 'url')(spec)
+}
 
-export const json = makeValidator((x: string) => {
-  try {
-    return JSON.parse(x)
-  } catch (e) {
-    throw new EnvError(`Invalid json: "${x}"`)
-  }
-}, 'json')
+export function json<T = any>(spec?: Spec<T>) {
+  return makeValidator((x: string) => {
+    try {
+      return JSON.parse(x)
+    } catch (e) {
+      throw new EnvError(`Invalid json: "${x}"`)
+    }
+  }, 'json')(spec)
+}
