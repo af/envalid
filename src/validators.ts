b2e1e545971507758bdaf74b578e44add652a745
Aaron Franks <aaron.franks@gmail.com>
2025-10-13 14:03:49 -0700

    Simplify ipv6 regex (trading off readability)

--- src/validators.ts
+++ src/validators.ts
@@ -14,23 +14,10 @@ const isFQDN = (input: string) => {
   return true
 }
 
-// Sourced from node.js standard library checks.
-// https://github.com/nodejs/node/blob/e105e821e9bc86fbd6e43577b4fcfb46eff8caf3/lib/internal/net.js#L15-L31
-const v4Seg = '(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])';
-const v4Str = `(?:${v4Seg}\\.){3}${v4Seg}`;
-const ipv4Regex = new RegExp(`^${v4Str}$`);
-
-const v6Seg = '(?:[0-9a-fA-F]{1,4})';
-const ipv6Regex = new RegExp('^(?:' +
-  `(?:${v6Seg}:){7}(?:${v6Seg}|:)|` +
-  `(?:${v6Seg}:){6}(?:${v4Str}|:${v6Seg}|:)|` +
-  `(?:${v6Seg}:){5}(?::${v4Str}|(?::${v6Seg}){1,2}|:)|` +
-  `(?:${v6Seg}:){4}(?:(?::${v6Seg}){0,1}:${v4Str}|(?::${v6Seg}){1,3}|:)|` +
-  `(?:${v6Seg}:){3}(?:(?::${v6Seg}){0,2}:${v4Str}|(?::${v6Seg}){1,4}|:)|` +
-  `(?:${v6Seg}:){2}(?:(?::${v6Seg}){0,3}:${v4Str}|(?::${v6Seg}){1,5}|:)|` +
-  `(?:${v6Seg}:){1}(?:(?::${v6Seg}){0,4}:${v4Str}|(?::${v6Seg}){1,6}|:)|` +
-  `(?::(?:(?::${v6Seg}){0,5}:${v4Str}|(?::${v6Seg}){1,7}|:))` +
-')(?:%[0-9a-zA-Z-.:]{1,})?$');
+// "best effort" regex-based IP address check
+// If you want a more exhaustive check, create your own custom validator
+const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])){3}$/
+const ipv6Regex = /^(([0-9a-f]{1,4}:){7}[0-9a-f]{1,4}|([0-9a-f]{1,4}:)+:([0-9a-f]{1,4}:)*[0-9a-f]{1,4}|([0-9a-f]{1,4}:)*::([0-9a-f]{1,4}:)*[0-9a-f]{0,4}|::|(([0-9a-f]{1,4}:){1,6}|:):((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9]))(%[0-9a-z.\-:]+)?$/i
 
 const isIP = (input: string) => {
   if (!input.length) return false
