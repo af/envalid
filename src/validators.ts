061b888986c476d46fe707f3a19df77b063ac01f
Szymon Łągiewka <slagiewka@users.noreply.github.com>
2025-10-17 04:46:26 +0200

    feat: improve IP RegExp to cover more cases (#239)
    * feat: improve IP RegExp to cover more cases
    
    This now covers cases such as the unspecified IPv6 address, NAT64
    mapping as well as scoped and zoned IPv6 addresses.
    
    Closes: #238
    
    * Simplify ipv6 regex (trading off readability)
    
    ---------
    
    Co-authored-by: Aaron Franks <aaron.franks@gmail.com>

--- src/validators.ts
+++ src/validators.ts
@@ -15,11 +15,10 @@ const isFQDN = (input: string) => {
 }
 
 // "best effort" regex-based IP address check
-// If you want a more exhaustive check, create your own custom validator, perhaps wrapping this
-// implementation (the source of the ipv4 regex below): https://github.com/validatorjs/validator.js/blob/master/src/lib/isIP.js
-const ipv4Regex =
-  /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
-const ipv6Regex = /([a-f0-9]+:+)+[a-f0-9]+/
+// If you want a more exhaustive check, create your own custom validator
+const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])){3}$/
+const ipv6Regex = /^(([0-9a-f]{1,4}:){7}[0-9a-f]{1,4}|([0-9a-f]{1,4}:)+:([0-9a-f]{1,4}:)*[0-9a-f]{1,4}|([0-9a-f]{1,4}:)*::([0-9a-f]{1,4}:)*[0-9a-f]{0,4}|::|(([0-9a-f]{1,4}:){1,6}|:):((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9]))(%[0-9a-z.\-:]+)?$/i
+
 const isIP = (input: string) => {
   if (!input.length) return false
   return ipv4Regex.test(input) || ipv6Regex.test(input)
